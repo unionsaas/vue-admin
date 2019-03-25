8b67e28d1cfdcf41fe4c5f2e24c2f051e2ac0bca
Zengs <zingsono@gmail.com>
2019-03-25 23:45:52 +0800

    admin

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello">
+      Dashboard 1111111111111111111111111
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Dashboard',
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="home">
+    <div class="header"></div>
+    <el-container>
+        <el-aside>
+            <el-menu :default-active="activeIndex" class="el-menu-vertical-demo"
+                     @open="handleOpen"
+                     @close="handleClose">
+                <el-menu-item index="1"><router-link to="/dashboard">仪表盘</router-link></el-menu-item>
+                <el-menu-item index="3">项目配置</el-menu-item>
+                <el-submenu index="4">
+                    <template slot="title">开放平台</template>
+                    <el-menu-item index="4-1">应用管理</el-menu-item>
+                    <el-menu-item index="4-2">项目管理</el-menu-item>
+                    <el-menu-item index="4-3">查看日志</el-menu-item>
+                </el-submenu>
+            </el-menu>
+        </el-aside>
+        <el-main style="background-color: aliceblue">
+            <router-view></router-view>
+        </el-main>
+    </el-container>
+    </div>
 </template>
+<style>
+    html,body,#app,#home,.el-container{height: 100%;}
+    #home{}
+    .el-container{position: relative; top: -60px;padding-top: 60px;}
+</style>
+<style scoped>
+    .header{height: 60px;background-color: #408be8;}
+    .avatar{ height: 60px;background-color: aquamarine;}
+    .router-link-active{    text-decoration: none;color: #303133;}
+
+</style>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    data() {
+        return {
+            activeIndex: '1',
+            activeIndex2: '1'
+        }
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath)
+        },
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath)
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath)
+        }
+    }
+
 }
 </script>
